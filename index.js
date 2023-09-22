a9cd2d5d8faf7da15187b2d79b8ca761acffc8b9
pest2811@gmail.com <pest2811@gmail.com>
2023-09-23 01:19:17 +0300

    set up connection to db

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+require('dotenv').config();
+const mysql = require('mysql2');
 
 const PORT = 5000;
 const app = express();
+app.use(express.json())
 
-app.get("/", (req, res) => {
+const connection = mysql.createConnection({
+  host     : process.env.HOST,
+  user     : process.env.USER,
+  password : process.env.PASSWORD,
+  database : process.env.DB
+}).promise();
+
+;
+app.post("/", (req, res) => {
     res.status(200).json("server works")
 });
 
-app.listen(PORT, () => console.log('server started on port: ', PORT));
+const startApp = async () => {
+    try {
+        const result = await connection.query("select * from object");
+        app.listen(PORT, () => console.log('server started on port: ', PORT));
+        console.log(result)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startApp()
